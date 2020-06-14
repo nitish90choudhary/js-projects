35709fda1f5dad467e081bddf05a91adb3e80c02
nitish.choudhary <nitish.choudhary@servicenow.com>
2020-06-14 20:40:01 +0200

    Fixed new product creation styling

--- e-comm/views/admin/products/new.js
+++ e-comm/views/admin/products/new.js
@@ -1,24 +1,35 @@
-const layout = require('../layout');
-const {
-    getError
-} = require('../../helper');
+const layout = require("../layout");
+const { getError } = require("../../helpers");
 
-module.exports = ({
-    errors
-}) => {
-    return layout({
-        content: `
-        <form method="POST" enctype="multipart/form-data">
-            <div>
-                <input name="productName" placeholder="Title"/ >
-                ${getError(errors,'productName')}
-                <input name="price" placeholder="Price"/ >
-                ${getError(errors,'price')}
-                <input name="image"  type="file"/ >
-                <button>Create Product</button>
+module.exports = ({ errors }) => {
+  return layout({
+    content: `
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="subtitle">Create a Product</h1>
+
+          <form method="POST" enctype="multipart/form-data">
+            <div class="field">
+              <label class="label">Title</label>
+              <input class="input" placeholder="Title" name="title">
+              <p class="help is-danger">${getError(errors, "title")}</p>
             </div>
-        </form>
-        `
-    })
-
+            
+            <div class="field">
+              <label class="label">Price</label>
+              <input class="input" placeholder="Price" name="price">
+              <p class="help is-danger">${getError(errors, "price")}</p>
+            </div>
+            
+            <div class="field">
+              <label class="label">Image</label>            
+              <input type="file" name="image" />
+            </div>
+            <br />
+            <button class="button is-primary">Create</button>
+          </form>
+        </div>
+      </div>
+    `,
+  });
 };

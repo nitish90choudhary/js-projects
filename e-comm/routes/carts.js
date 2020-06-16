89581ccc565dd9975281aee3067b28b10a364212
nitish.choudhary <nitish.choudhary@servicenow.com>
2020-06-16 21:42:13 +0200

    Finished cart logic

--- e-comm/routes/carts.js
+++ e-comm/routes/carts.js
@@ -1,52 +1,53 @@
 const express = require("express");
 const cartsRepo = require("../data/carts");
+const productsRepo = require("../data/products");
 const router = express.Router();
 const productsIndexTemplate = require("../views/products/index");
+const cartTemplate = require("../views/cart/index");
 
 //1 recive a post request to add product to cart
 router.post("/cart/products/:productId/add", async (req, res) => {
   //New cart
+  let cart;
   if (!req.session.cartId) {
-    const items = [];
-    items.push({ id: req.params.productId, quantity: 1 });
-    const cart = await cartsRepo.create({ items });
+    cart = await cartsRepo.create({ items: [] });
     req.session.cartId = cart.id;
-    return res.send("cart created");
-  }
+  } else cart = await cartsRepo.getOne(req.session.cartId);
 
-  console.log("session", req.session.cartId);
-  const existingCart = await cartsRepo.getOne(req.session.cartId);
-
-  //If product is already in cart then increment the quantity
-  //else add it
-  const elementPresence = existingCart.items.findIndex(
-    (ele) => ele.id == req.params.productId
+  const elementPresence = cart.items.findIndex(
+    (ele) => ele.id === req.params.productId
   );
-  console.log("presence", elementPresence);
 
   if (elementPresence >= 0) {
     console.log("Item alredy in the cart");
-    existingCart.items[elementPresence].quantity = existingCart.items[elementPresence].quantity  + 1;
-    await cartsRepo.update(existingCart.id, existingCart);
+    cart.items[elementPresence].quantity++;
   } else {
     console.log("new Item adding in the cart");
-    existingCart.items.push({ id: req.params.productId, quantity: 1 });
-    await cartsRepo.update(existingCart.id, existingCart);
+    cart.items.push({ id: req.params.productId, quantity: 1 });
   }
-
-  // await cartsRepo.update(cart.id,{})
-  console.log(existingCart);
+  await cartsRepo.update(cart.id, cart);
+  res.redirect("/cart");
 });
 
 //2 recive a post request to delete product to cart
-router.get("/cart/products/:productId/delete", async (req, res) => {
-  req.session = null;
-  res.send("deleted");
+router.post("/cart/products/:itemId/delete", async (req, res) => {
+  console.log(req.params.itemId);
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  const items = cart.items.filter((item) => item.id !== req.params.itemId);
+  await cartsRepo.update(cart.id, { items });
+  console.log(items);
+  res.redirect("/cart");
 });
 
 //3. Get request to show all the items added in cart
 router.get("/cart", async (req, res) => {
-  res.send("cart displayed");
+  if (!req.session.cartId) res.redirect("/");
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    item.product = product;
+  }
+  res.send(cartTemplate({ items: cart.items }));
 });
 
 module.exports = router;
